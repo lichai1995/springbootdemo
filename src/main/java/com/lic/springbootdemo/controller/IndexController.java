db72177b4088563605c0d22402625491e3b4a672
lichai <lichai_6321@163.com>
2019-07-15 09:29:10 +0800

    second commit

---
+++ src/main/java/com/lic/springbootdemo/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.lic.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lichai
+ * @date 9:17 2019/7/15
+ */
+@RestController
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String index(){
+        return "Hello World!";
+    }
+}
